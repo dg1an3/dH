0588ecc42896cba4a1bf70a96d6cbe2f044a897b
dlane <>
2001-01-24 17:07:08 +0000

    Initial import of the VSIM_OGL prototype module

---
+++ VSIM_OGL/StdAfx.cpp
@@ -0,0 +1,8 @@
+// stdafx.cpp : source file that includes just the standard includes
+//	VSIM_OGL.pch will be the pre-compiled header
+//	stdafx.obj will contain the pre-compiled type information
+
+#include "stdafx.h"
+
+
+
