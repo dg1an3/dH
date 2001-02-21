dfa86a65783d9b9272ff911f4f587e02d97826e4
dlane <>
2001-02-21 17:33:50 +0000

    Initial import of the VSIM_OGL prototype module

---
+++ VSIM_OGL/resource.h
@@ -0,0 +1,75 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by VSIM_OGL.rc
+//
+#define IDD_ABOUTBOX                    100
+#define IDR_MAINFRAME                   128
+#define IDR_VSIM_OTYPE                  129
+#define IDD_TABCONTROLBAR               130
+#define IDD_STRUCTUREDIALOG             131
+#define IDD_BEAMPARAMCOLLIM             133
+#define IDD_BEAMPARAMPOSITION           134
+#define IDD_OBJECTEXPLORERCONTROLBAR    135
+#define IDB_POINT_YELLOW                135
+#define IDB_BEAM_MAGENTA                136
+#define IDB_CONTOUR_BLUE                137
+#define IDB_CONTOUR_GREEN               138
+#define IDB_CONTOUR_RED                 139
+#define IDB_CONTOUR_YELLOW              140
+#define IDB_FOLDER_CLOSED               141
+#define IDB_FOLDER_OPENED               142
+#define IDB_POINT_BLUE                  143
+#define IDB_POINT_GREEN                 144
+#define IDB_POINT_RED                   145
+#define IDB_BEAM_GREEN                  146
+#define IDB_CONTOUR_GREY                147
+#define IDB_CONTOUR_MAGENTA             148
+#define IDB_CONTOUR_CYAN                149
+#define IDC_EDIT_GANTRY                 1001
+#define IDC_EDIT_COLLIM                 1001
+#define IDC_EDIT_COUCH                  1002
+#define IDC_EDIT_TABLEX                 1003
+#define IDC_EDIT_TABLEY                 1004
+#define IDC_EDIT_TABLEZ                 1005
+#define IDC_EDIT_JAWX1                  1006
+#define IDC_EDIT_JAWX2                  1007
+#define IDC_EDIT_JAWY1                  1008
+#define IDC_EDIT_JAWY2                  1009
+#define IDC_TABCTRL                     1010
+#define IDC_CHILDRECT                   1011
+#define IDC_SPIN_TABLEX                 1012
+#define IDC_SPIN_COUCH                  1013
+#define IDC_SPIN_GANTRY                 1014
+#define IDC_SPIN_TABLEY                 1015
+#define IDC_SPIN_TABLEZ                 1016
+#define IDC_SPIN_COLLIM                 1016
+#define IDC_CHECK1                      1017
+#define IDC_CHECK2                      1018
+#define IDC_SPIN_JAWX1                  1019
+#define IDC_CHECK3                      1019
+#define IDC_TREECTRLRECT                1019
+#define IDC_SPIN_JAWX2                  1020
+#define IDC_CHECK4                      1020
+#define IDC_SPIN_JAWY1                  1021
+#define IDC_CHECK5                      1021
+#define IDC_SPIN_JAWY2                  1022
+#define IDC_CHECK6                      1022
+#define IDC_CHECK7                      1023
+#define IDC_CHECK8                      1024
+#define ID_VIEW_BEAMPARAM               32771
+#define ID_VIEW_LIGHTPATCH              32772
+#define ID_VIEW_BEAM                    32773
+#define ID_VIEW_STRUCTURE               32775
+#define ID_VIEW_WIREFRAME               32776
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_3D_CONTROLS                     1
+#define _APS_NEXT_RESOURCE_VALUE        147
+#define _APS_NEXT_COMMAND_VALUE         32777
+#define _APS_NEXT_CONTROL_VALUE         1020
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
