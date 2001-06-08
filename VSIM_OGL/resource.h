e49244794c2a90692bdfc55355d70d978b774dba
administrator <>
2001-06-08 22:51:52 +0000

    Backed-out of resource changes

--- VSIM_OGL/resource.h
+++ VSIM_OGL/resource.h
@@ -60,7 +60,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_3D_CONTROLS                     1
-#define _APS_NEXT_RESOURCE_VALUE        147
+#define _APS_NEXT_RESOURCE_VALUE        152
 #define _APS_NEXT_COMMAND_VALUE         32777
 #define _APS_NEXT_CONTROL_VALUE         1020
 #define _APS_NEXT_SYMED_VALUE           101
