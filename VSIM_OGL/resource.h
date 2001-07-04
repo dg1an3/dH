0ac819a250e8fd7c1454abe5e649922920696e62
DLane <>
2001-07-04 00:14:26 +0000

    Added colorwash control to the toolbar

--- VSIM_OGL/resource.h
+++ VSIM_OGL/resource.h
@@ -54,6 +54,8 @@
 #define ID_VIEW_BEAM                    32773
 #define ID_VIEW_STRUCTURE               32775
 #define ID_VIEW_WIREFRAME               32776
+#define ID_VIEW_COLORWASH               32777
+#define ID_BUTTON32778                  32778
 
 // Next default values for new objects
 // 
@@ -61,7 +63,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_3D_CONTROLS                     1
 #define _APS_NEXT_RESOURCE_VALUE        152
-#define _APS_NEXT_COMMAND_VALUE         32777
+#define _APS_NEXT_COMMAND_VALUE         32779
 #define _APS_NEXT_CONTROL_VALUE         1020
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
