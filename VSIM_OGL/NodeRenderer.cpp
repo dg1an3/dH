a1cfa0792bd09f87fba726a01825108473e09d42
DLane <>
2001-08-08 22:55:57 +0000

    Enhancing node renderer (prior to moving to a different module)

--- VSIM_OGL/NodeRenderer.cpp
+++ VSIM_OGL/NodeRenderer.cpp
@@ -11,18 +11,18 @@
 #include <glMatrixVector.h>
 
 
-const int NUM_STEPS = 20;
-const double RADIUS = 5.0;
+const int NUM_STEPS = 4;
 
 class CMolding 
 {
 public:
 	CMolding();
-	CMolding(double theta, CVector<3> vOffset);
+	CMolding(double theta, CVector<3> vOffset, double radius);
 
-	static CMolding g_masterMolding;
 	CVector<3> m_arrVertices[NUM_STEPS + 1];
 	CVector<3> m_arrNormals[NUM_STEPS + 1];
+
+	static CMolding g_masterMolding;
 };
 
 CMolding CMolding::g_masterMolding;
@@ -42,13 +42,13 @@ CMolding::CMolding()
 		nAt++;
 	}
 
-	m_arrVertices[nAt][0] = -2.0;
+	m_arrVertices[nAt][0] = -4.5;
 	m_arrVertices[nAt][2] = 1.0;
 
 	m_arrNormals[nAt][2] = 1.0;
 }
 
-CMolding::CMolding(double theta, CVector<3> vOffset)
+CMolding::CMolding(double theta, CVector<3> vOffset, double radius)
 {
 	CMatrix<4> mRotate = CreateRotate(theta, CVector<3>(0.0, 0.0, 1.0));
 	for (int nAt = 0; nAt < NUM_STEPS+1; nAt++)
@@ -58,7 +58,7 @@ CMolding::CMolding(double theta, CVector<3> vOffset)
 				g_masterMolding.m_arrNormals[nAt]));
 
 		m_arrVertices[nAt] = 
-			RADIUS * FromHomogeneous<3, double>(mRotate * ToHomogeneous(
+			radius * FromHomogeneous<3, double>(mRotate * ToHomogeneous(
 				g_masterMolding.m_arrVertices[nAt]));
 
 		m_arrVertices[nAt] += vOffset;
@@ -70,10 +70,18 @@ CMolding::CMolding(double theta, CVector<3> vOffset)
 //////////////////////////////////////////////////////////////////////
 
 CNodeRenderer::CNodeRenderer(COpenGLView *pView)
-	: COpenGLRenderer(pView)
+	: COpenGLRenderer(pView),
+		borderRadius(10.0),
+		rectangularity(1.0 / sqrt(2.0)),
+		width(25.0),
+		aspectRatio(0.625)
+		// color(RGB(192, 192, 192))
 {
 	color.Set(RGB(192, 192, 192));
 
+	activation.AddObserver(this, (ChangeFunction) OnActivationChanged);
+	rectangularity.AddObserver(this, (ChangeFunction) OnChange);
+	width.AddObserver(this, (ChangeFunction) OnChange);
 }
 
 CNodeRenderer::~CNodeRenderer()
@@ -81,143 +89,32 @@ CNodeRenderer::~CNodeRenderer()
 
 }
 
-/////////////////////////////////////////////////////////////////////////////
-// CNodeView::GetOuterRect
-//
-//
-/////////////////////////////////////////////////////////////////////////////
-CRect CNodeRenderer::GetOuterRect()
+
+CVector<3> CNodeRenderer::EvalEllipseMolding(double theta, double a, double b)
 {
-	// outer rectangle = client rectangle
-	// CRect rectOuter;
-	// GetClientRect(&rectOuter);
-
-	return m_rectOuter;
-}
-
-
-/////////////////////////////////////////////////////////////////////////////
-// CNodeView::GetInnerRect
-//
-//
-/////////////////////////////////////////////////////////////////////////////
-CRect CNodeRenderer::GetInnerRect()
-{
-	CRect rectOuter = GetOuterRect();
-
-	// compute the r, which represents the amount of "elliptical-ness"
-	// float r = 1.0f - (1.0f - 1.0f / (float) sqrt(2.0f))
-	// 	* (float) exp(-8.0f * privActivation.Get());
-
-	CRect rectInner = rectOuter;
-	rectInner.top = (long) ((float) (rectOuter.top + rectOuter.Height() / 2) 
-		- m_r * (float) rectOuter.Height() / 2.0f);
-	rectInner.bottom = (long) ((float) (rectOuter.top + rectOuter.Height() / 2) 
-		+ m_r * (float) rectOuter.Height() / 2.0f);
-	rectInner.left = (long) ((float) (rectOuter.left + rectOuter.Width() / 2) 
-		- m_r * (float) rectOuter.Width() / 2.0f);
-	rectInner.right = (long) ((float) (rectOuter.left + rectOuter.Width() / 2) 
-		+ m_r * (float) rectOuter.Width() / 2.0f);
-
-	return rectInner;
-}
-
-
-/////////////////////////////////////////////////////////////////////////////
-// CNodeView::GetLeftRightEllipseRect
-//
-//
-/////////////////////////////////////////////////////////////////////////////
-CRect CNodeRenderer::GetLeftRightEllipseRect()
-{
-	CRect rectOuter = GetOuterRect();
-	CRect rectInner = GetInnerRect();
-
-	// compute the ellipse's b
-	float rx = ((float) rectInner.Width())
-			/ ((float) rectOuter.Width()); 
-	float bx = (float) sqrt((float) (rectInner.Height() 
-		* rectInner.Height()) * 0.25f
-			/ (1.0f - rx * rx));
-
-	// now compute the ellipse's rectangle
-	CRect rectLeftRightEllipse = rectOuter;
-	rectLeftRightEllipse.top = (rectOuter.top + rectOuter.Height() / 2) - (long) bx;
-	rectLeftRightEllipse.bottom = (rectOuter.top + rectOuter.Height() / 2) + (long) bx;
-
-	return rectLeftRightEllipse;
-}
-
-
-/////////////////////////////////////////////////////////////////////////////
-// CNodeView::GetTopBottomEllipseRect
-//
-//
-/////////////////////////////////////////////////////////////////////////////
-CRect CNodeRenderer::GetTopBottomEllipseRect()
-{
-	CRect rectOuter = GetOuterRect();
-	CRect rectInner = GetInnerRect();
-
-	// compute the ellipse's b
-	float ry = ((float) rectInner.Height())
-			/ ((float) rectOuter.Height()); 
-	float by = (float) sqrt((float) (rectInner.Width() * rectInner.Width()) * 0.25f
-			/ (1.0f - ry * ry));
-
-	// now compute the ellipse's rectangle
-	CRect rectTopBottomEllipse = rectOuter;
-	rectTopBottomEllipse.left = (rectOuter.left + rectOuter.Width() / 2) - (long) by;
-	rectTopBottomEllipse.right = (rectOuter.left + rectOuter.Width() / 2) + (long) by;
-
-	return rectTopBottomEllipse;
-}
-
-
-CVector<3> EvalShape2D(double theta)
-{
-	double a = 12.0;
-	double b = 48.0;
-
-	if ((theta > PI / 4 && theta < 3 * PI / 4) ||
-		 (theta > 5 * PI / 4 && theta < 7 * PI / 4))
-	{
-		a = 48.0;
-		b = 12.0;
-	}
-
 	// compute theta_prime for the ellipse
 	double sign = cos(theta) > 0 ? 1.0 : -1.0;
 	double theta_prime = atan2(sign * a * tan(theta), sign * b);
 
 	return CVector<3>(a * cos(theta_prime), b * sin(theta_prime), 0.0);
+
 }
 
-void CNodeRenderer::OnRenderScene()
+CMolding *CNodeRenderer::RenderMoldingSection(double angle_start, double angle_stop,
+										 double a, double b, CMolding *pOldMolding)
 {
+	if (pOldMolding == NULL)
+		pOldMolding = 
+			new CMolding(angle_start, EvalEllipseMolding(angle_start, a, b), borderRadius.Get());
 
-	glColor(color.Get());
-
-	// glTranslated(150.0, 100.0, 0.0);
-
-	// draw an elliptangle
-/*	glBegin(GL_QUADS);
-		glNormal3d(0.0, 1.0, 0.0);
-		glVertex3d(-50.0, 5.0, -50.0);
-		glVertex3d(-50.0, 5.0,  50.0);
-		glVertex3d( 50.0, 5.0,  50.0);
-		glVertex3d( 50.0, 5.0, -50.0);
-	glEnd();
-*/
-	double step = PI / 20.0;
-	CMolding *pOldMolding = new CMolding(0.0, EvalShape2D(0.0));
-	for (double theta = 0.0; theta <= (2 * PI + step); theta += step)
+	double step = (angle_stop - angle_start) / 8.0;
+	for (double theta = angle_start; theta <= angle_stop; theta += step)
 	{
 		// create the molding from the template
-		CVector<3> vShapePt = EvalShape2D(theta);
+		CVector<3> vShapePt = EvalEllipseMolding(theta, a, b);
 
 		// create a new molding object
-		CMolding *pNewMolding = new CMolding(theta, vShapePt);
+		CMolding *pNewMolding = new CMolding(theta, vShapePt, borderRadius.Get());
 
 		for (int nAt = 0; nAt < NUM_STEPS; nAt++)
 		{
@@ -241,4 +138,64 @@ void CNodeRenderer::OnRenderScene()
 		delete pOldMolding;
 		pOldMolding = pNewMolding;
 	}
+
+	return pOldMolding;
+}
+
+void CNodeRenderer::OnRenderScene()
+{
+	// set the color for the node
+	glColor(color.Get());
+
+	// compute the base ellipse a and b
+	double a = width.Get();
+	double b = a * aspectRatio.Get();
+
+	// draw an elliptangle
+	glBegin(GL_QUADS);
+		glNormal3d(  0.0, 0.0,   1.0);
+		glVertex3d(-a * rectangularity.Get(), -b * rectangularity.Get(), borderRadius.Get());
+		glVertex3d( a * rectangularity.Get(), -b * rectangularity.Get(), borderRadius.Get());
+		glVertex3d( a * rectangularity.Get(),  b * rectangularity.Get(), borderRadius.Get());
+		glVertex3d(-a * rectangularity.Get(),  b * rectangularity.Get(), borderRadius.Get());
+	glEnd();
+
+	// compute the angle dividing the sections of the elliptangle
+	double sectionAngle = atan2(b, a);
+
+	// computing the a and b adjustments for the sections of the elliptangle
+	double r_factor = rectangularity.Get()
+		/ sqrt(1.0 - rectangularity.Get() * rectangularity.Get());
+
+	// now render the four sections of the molding
+	CMolding* pOld;
+	pOld = RenderMoldingSection(2*PI,                2*PI + sectionAngle, 
+		a, b * r_factor);
+	pOld = RenderMoldingSection(       sectionAngle,   PI - sectionAngle, 
+		a * r_factor, b, pOld);
+	pOld = RenderMoldingSection(  PI - sectionAngle,   PI + sectionAngle, 
+		a, b * r_factor, pOld);
+	pOld = RenderMoldingSection(  PI + sectionAngle, 2*PI - sectionAngle, 
+		a * r_factor, b, pOld);
+	pOld = RenderMoldingSection(2*PI - sectionAngle, 2*PI + 0.1, 
+		a, b * r_factor, pOld);
+
+	// delete the connecting molding
+	delete pOld;
+}
+
+void CNodeRenderer::OnActivationChanged(CObservableObject *pSource, void *pOldValue)
+{
+	// set the new width for the node renderer
+	width.Set(50.0 + 200.0 * sqrt(activation.Get() / aspectRatio.Get()));
+
+	// set the new rectangularity for the shape
+	const double c = 0.88 - 1.0 / sqrt(2.0);
+	rectangularity.Set((activation.Get() * c) + 1.0 / sqrt(2.0));
+
+	// set the aspect ratio for the renderer
+	aspectRatio.Set(0.75 - 0.375 * exp(-8.0 * activation.Get()));
+
+	// set the new border radius
+	borderRadius.Set(8.0 + sqrt(activation.Get()) * 10.0);
 }
--- VSIM_OGL/NodeRenderer.h
+++ VSIM_OGL/NodeRenderer.h
@@ -14,6 +14,8 @@
 
 #include "OpenGLRenderer.h"
 
+class CMolding;
+
 class CNodeRenderer : public COpenGLRenderer  
 {
 public:
@@ -30,30 +32,27 @@ public:
 	// stores the node's aspect ratio
 	CValue< double > aspectRatio;
 
+	// stores the radius of the border
+	CValue< double > borderRadius;
+
 	// association to the node to be rendered
 	// CAssociation< CNode > forNode;
 
 protected:
-	// computes the node's size (X and Y)
-	CValue< CVector<2> > size;
+	// computes the node's x-size (ellipse a)
+	CValue< double > width;
 
 	// computes the node's rectangularity (from sqrt(2.0e) to 1.0))
 	CValue< double > rectangularity;
 
-	// accessor for the inner and outer rectangles 
-	//		(defines the shape of the node view)
-	CRect GetOuterRect();
-	CRect GetInnerRect();
-
-	// accessors for the left-right and top-bottom ellipse rectangles
-	CRect GetTopBottomEllipseRect();
-	CRect GetLeftRightEllipseRect();
-
+	// evaluates the elliptangle at a point
+	CVector<3> EvalEllipseMolding(double theta, double a, double b);
+	CMolding *RenderMoldingSection(double angle_start, double angle_stop, 
+		double a, double b, CMolding *pOld = NULL);
 	virtual void OnRenderScene();
 
-private:
-	double m_r;
-	CRect m_rectOuter;
+	// handler for activation change
+	void OnActivationChanged(CObservableObject *pSource, void *pOldValue);
 };
 
 #endif // !defined(AFX_NODERENDERER_H__35348172_09C8_401B_A1D6_955CE7CE536D__INCLUDED_)
