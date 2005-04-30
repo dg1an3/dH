050022e7058000c6e6eb4696a82299d0f6530a3a
HP_Administrator <>
2005-04-30 20:27:31 +0000

    Integrated MPR view

---
+++ Brimstone/PlanarView.cpp
@@ -0,0 +1,597 @@
+// PlanarView.cpp : implementation file
+//
+
+#include "stdafx.h"
+#include "brimstone.h"
+#include "PlanarView.h"
+
+#include <Series.h>
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#undef THIS_FILE
+static char THIS_FILE[] = __FILE__;
+#endif
+
+/////////////////////////////////////////////////////////////////////////////
+// CPlanarView
+
+CPlanarView::CPlanarView()
+: m_bWindowLeveling(FALSE),
+	m_pSeries(NULL)
+{
+	m_pVolume[0] = NULL;
+	m_pVolume[1] = NULL;
+
+	m_window[0] = 2.0;
+	m_window[1] = 1.0 / 0.8;
+
+	m_level[0] = 1.0;
+	m_level[1] = 0.4;
+
+	m_alpha = 1.0;
+}
+
+CPlanarView::~CPlanarView()
+{
+}
+
+
+BEGIN_MESSAGE_MAP(CPlanarView, CWnd)
+	//{{AFX_MSG_MAP(CPlanarView)
+	ON_WM_PAINT()
+	ON_WM_MBUTTONDOWN()
+	ON_WM_MBUTTONUP()
+	ON_WM_MOUSEMOVE()
+	ON_WM_SIZE()
+	ON_WM_CREATE()
+	//}}AFX_MSG_MAP
+END_MESSAGE_MAP()
+
+
+/////////////////////////////////////////////////////////////////////////////
+// CPlanarView message handlers
+
+void CPlanarView::SetVolume(CVolume<REAL> *pVolume, int nVolumeAt)
+{
+	if (nVolumeAt == 1 && m_pVolume[1] == NULL)
+	{
+		m_alpha = 0.75;
+	}
+
+	m_pVolume[nVolumeAt] = pVolume;
+}
+
+void CPlanarView::SetBasis(const CMatrixD<4>& mBasis)
+{
+	m_volumeResamp[0].SetBasis(mBasis);
+	m_volumeResamp[1].SetBasis(mBasis);
+}
+
+void CPlanarView::SetWindowLevel(REAL win, REAL cen, int nVolumeAt)
+{
+	m_window[nVolumeAt] = win;
+	m_level[nVolumeAt] = cen;
+}
+
+void CPlanarView::SetAlpha(REAL alpha)
+{
+	m_alpha = alpha;
+}
+
+void CPlanarView::SetLUT(CArray<COLORREF, COLORREF>& arrLUT, int nVolumeAt)
+{
+	m_arrLUT[nVolumeAt].Copy(arrLUT);
+}
+
+void CPlanarView::AddStructure(CStructure *pStruct)
+{
+	m_arrStructures.Add(pStruct);
+}
+
+void CPlanarView::OnPaint() 
+{
+	CPaintDC dc(this); // device context for painting
+
+	CRect rect;
+	GetClientRect(&rect);
+
+	DrawImages(&dc);
+
+	CDC dcMem;
+	dcMem.CreateCompatibleDC(&dc);
+
+	// selects image objects in to context
+	dcMem.SelectObject(m_dib);
+
+	dcMem.SelectStockObject(HOLLOW_BRUSH);
+	dcMem.Rectangle(rect);
+
+	// DrawContours(pDC, rect, &density);
+	DrawContours(&dcMem);
+	
+	if (m_pVolume[1] != NULL && m_pVolume[1]->GetHeight() > 0)
+	{
+		REAL c = 0.4;
+		while (c < 1.0)
+		{
+			REAL max_value = m_arrLUT[1].GetSize();
+			REAL pix_value1 = max_value * m_window[1] * (c - m_level[1]) + max_value / 2.0;
+
+			// scale to 0..255
+			pix_value1 = __min(pix_value1, max_value - 1.0);
+			pix_value1 = __max(pix_value1, 0.0);
+
+			//  colorIndex = (int) pix_value1 // * (REAL) (max_value - 1.0));
+			int colorIndex = __min(pix_value1, max_value - 1.0);
+
+			CPen pen(PS_SOLID, 1, m_arrLUT[1][colorIndex]);
+			dcMem.SelectObject(&pen);
+			DrawIsocurves(m_pVolume[1], c, &dcMem);
+
+			c += 0.05;
+		}
+	}
+
+	dc.BitBlt(0, 0, rect.Width(), rect.Height(), &dcMem, 0, 0, SRCCOPY);
+
+	// Do not call CWnd::OnPaint() for painting messages
+}
+
+void CPlanarView::DrawImages(CDC *pDC)
+{
+	BOOL bDraw = FALSE;
+
+	CRect rect;
+	GetClientRect(&rect);
+
+	REAL *pVoxels0 = NULL;
+	if (m_pVolume[0] && m_pVolume[0]->GetWidth() != 0)
+	{
+		if (m_volumeResamp[0].GetWidth() != rect.Width())
+		{
+			m_volumeResamp[0].SetDimensions(rect.Width(), rect.Height(), 1);
+			
+			// FIX for actual width
+			rect.right = rect.left + m_volumeResamp[0].GetWidth();
+			ASSERT(rect.Width() == m_volumeResamp[0].GetWidth());
+			ASSERT(rect.Width() % 4 == 0);
+		}
+
+		CMatrixD<4> mBasis = m_pVolume[0]->GetBasis();
+		mBasis[0][0] = mBasis[1][1] = 0.5 * (REAL) m_pVolume[0]->GetHeight() / (REAL) rect.Height();
+		mBasis[3][0] += 0.25 * (REAL) m_pVolume[0]->GetHeight();
+		mBasis[3][1] += 0.25 * (REAL) m_pVolume[0]->GetHeight();
+		m_volumeResamp[0].SetBasis(mBasis);
+
+		m_volumeResamp[0].ClearVoxels();
+		Resample(m_pVolume[0], &m_volumeResamp[0], TRUE);
+
+		pVoxels0 = m_volumeResamp[0].GetVoxels()[0][0];
+
+		bDraw = TRUE;
+	}
+
+	REAL *pVoxels1 = NULL; 
+	if (m_pVolume[1] && m_pVolume[1]->GetHeight() != 0)
+	{
+		m_volumeResamp[1].ConformTo(&m_volumeResamp[0]);
+/*		if (m_volumeResamp[1].GetWidth() != rect.Width())
+		{
+			m_volumeResamp[1].SetDimensions(rect.Width(), rect.Height(), 1);
+		}
+		m_volumeResamp[1].SetBasis(m_volumeResamp[0].GetBasis());  */
+
+		m_volumeResamp[1].ClearVoxels();
+		Resample(m_pVolume[1], &m_volumeResamp[1], TRUE);
+
+		pVoxels1 = m_volumeResamp[1].GetVoxels()[0][0];
+		bDraw = TRUE;
+	}
+
+	if (!bDraw)
+		return;
+	
+	m_arrPixels.SetSize(rect.Width() * rect.Height());
+
+	REAL alpha1 = 1.0 - m_alpha;
+	for (int nAt = 0; nAt < rect.Width() * rect.Height(); nAt++)
+	{
+		REAL pix_value0 = 256.0 * m_window[0] * (pVoxels0[nAt] - m_level[0]) + 128.0;
+
+		// scale to 0..255
+		pix_value0 = __min(pix_value0, 255.0);
+		pix_value0 = __max(pix_value0, 0.0);
+
+		if (!pVoxels1)
+		{
+			m_arrPixels[nAt] = 
+				RGB(pix_value0, pix_value0, pix_value0);
+		}
+		else
+		{
+			REAL max_value = m_arrLUT[1].GetSize();
+			REAL pix_value1 = max_value * m_window[1] * (pVoxels1[nAt] - m_level[1]) + max_value / 2.0;
+
+			// scale to 0..255
+			pix_value1 = __min(pix_value1, max_value - 1.0);
+			pix_value1 = __max(pix_value1, 0.0);
+
+			//  colorIndex = (int) pix_value1 // * (REAL) (max_value - 1.0));
+			int colorIndex = __min(pix_value1, max_value - 1.0);
+			COLORREF color1 = m_arrLUT[1][colorIndex];
+
+			if (colorIndex > 5)
+			{
+				m_arrPixels[nAt] = 
+					RGB(
+						m_alpha * pix_value0 + alpha1 * GetBValue(color1), 
+						m_alpha * pix_value0 + alpha1 * GetGValue(color1),
+						m_alpha * pix_value0 + alpha1 * GetRValue(color1)
+						);
+			}
+			else
+			{
+				m_arrPixels[nAt] = 
+					RGB(pix_value0, pix_value0, pix_value0);
+			}
+		}
+	}
+
+	
+	if (m_dib.GetSize().cx != rect.Width())
+	{
+		m_dib.DeleteObject();
+		HBITMAP bm = ::CreateCompatibleBitmap(*pDC, rect.Width(), rect.Height());
+		m_dib.Attach(bm);
+	}
+	DWORD dwCount = m_dib.SetBitmapBits(rect.Width() * rect.Height() * sizeof(COLORREF),
+		(void *) m_arrPixels.GetData());
+
+	// PLDrawBitmap(*pDC, &m_dib, NULL, NULL, SRCCOPY);
+}
+
+
+
+void CPlanarView::OnMButtonDown(UINT nFlags, CPoint point) 
+{
+	m_bWindowLeveling = TRUE;
+	m_ptWinLevStart = point;
+	m_windowStart = m_window[0];
+	m_levelStart = m_level[0];
+	
+	SetCapture();
+
+	CWnd::OnMButtonDown(nFlags, point);
+}
+
+void CPlanarView::OnMButtonUp(UINT nFlags, CPoint point) 
+{
+	m_bWindowLeveling = FALSE;
+	::ReleaseCapture();
+
+	CWnd::OnMButtonUp(nFlags, point);
+}
+
+void CPlanarView::OnMouseMove(UINT nFlags, CPoint point) 
+{
+	if (m_bWindowLeveling)
+	{
+		CRect rectClient;
+		GetClientRect(&rectClient);
+
+		REAL voxelMax = m_pVolume[0]->GetMax();
+
+		CPoint ptDelta = point - m_ptWinLevStart;
+
+		m_window[0] = m_windowStart - voxelMax * (REAL) ptDelta.y / (REAL) rectClient.Height();
+		m_window[0] = __max(m_window[0], 0.001);
+
+		m_level[0] = m_levelStart - voxelMax * (REAL) ptDelta.x / (REAL) rectClient.Width();
+
+		// Invalidate(FALSE);
+		RedrawWindow(NULL, NULL, RDW_INVALIDATE | RDW_UPDATENOW);
+	}
+	
+	CWnd::OnMouseMove(nFlags, point);
+}
+
+CPoint ToDC(const CVectorD<2>& vVert, const CMatrixD<4>& mBasis)
+{
+	CPoint pt;
+		// (
+		// vVert[0] / (REAL) pDens->GetWidth() * (REAL) rect.Width(),
+		// vVert[1] / (REAL) pDens->GetHeight() * (REAL) rect.Height());
+	// pt += rect.TopLeft();
+	pt.x = (vVert[0] - mBasis[3][0]) / mBasis[0][0];
+	pt.y = (vVert[1] - mBasis[3][1]) / mBasis[1][1];
+
+	return pt;
+		
+}
+
+void CPlanarView::DrawContours(CDC *pDC)
+{
+	if (!m_pSeries)
+		return;
+
+	const CMatrixD<4>& mBasis = m_volumeResamp[0].GetBasis();
+	REAL slicePos = mBasis[3][2]; // (mBasis * ToHG(CVectorD<3>(0.0, 0.0, 0.0)))[2];
+
+	for (int nAtStruct = 0; nAtStruct < m_pSeries->GetStructureCount(); nAtStruct++)
+	{
+		CStructure *pStruct = m_pSeries->GetStructureAt(nAtStruct);
+
+		if (!pStruct->m_bVisible)
+			continue;
+
+		CPen pen(PS_SOLID, 2, pStruct->m_color);
+		CPen *pOldPen = pDC->SelectObject(&pen);
+		pDC->SelectStockObject(HOLLOW_BRUSH);
+
+		for (int nAtContour = 0; nAtContour < pStruct->GetContourCount(); nAtContour++)
+		{
+			REAL zPos = pStruct->GetContourRefDist(nAtContour);
+			if (IsApproxEqual(zPos, slicePos))
+			{
+				CPolygon *pPoly = pStruct->GetContour(nAtContour);
+
+				pDC->MoveTo(ToDC(pPoly->GetVertexAt(0), mBasis));
+				for (int nAtVert = 1; nAtVert < pPoly->GetVertexCount(); nAtVert++)
+				{
+					pDC->LineTo(ToDC(pPoly->GetVertexAt(nAtVert), mBasis));
+				}
+				pDC->LineTo(ToDC(pPoly->GetVertexAt(0), mBasis));
+			}
+		}
+
+		pDC->SelectObject(pOldPen);
+	}
+
+}
+
+
+
+double calcPoint(double bottom, double top, double c, double tempstep)
+{             
+	return ((c-bottom)/(top-bottom))*tempstep; 
+}
+
+
+void CPlanarView::DrawIsocurves(CVolume<REAL> *pVolume, REAL c, CDC *pDC)
+{
+	const CMatrixD<4>& mBasis = m_volumeResamp[0].GetBasis();
+
+	double topleft = 0;
+	double topright = 0;
+	double bottomleft = 0;
+	double bottomright = 0;
+	
+	REAL **ppPixels = pVolume->GetVoxels()[0];
+
+	CVectorD<3> vPixSpacing = pVolume->GetPixelSpacing();
+	CVectorD<3> vOrigin = FromHG<3, REAL>(pVolume->GetBasis()[3]);
+
+	for(int nY = 0; nY < pVolume->GetHeight()-1; nY++)
+	{          
+		for(int nX = 0; nX < pVolume->GetWidth()-1; nX++)
+		{           
+			if(nX == 0)
+			{
+				topleft = ppPixels[nY+1][nX];
+				topright = ppPixels[nY+1][nX+1];
+				bottomleft = ppPixels[nY][nX];
+				bottomright = ppPixels[nY][nX+1];
+			}
+			else
+			{
+				topleft = topright;
+				topright = ppPixels[nY+1][nX+1];
+				bottomleft = bottomright;
+				bottomright = ppPixels[nY][nX+1]; 
+			}         
+			
+			//case 1.1
+			if((topleft<c && topright<c && bottomleft>c && bottomright>c) 
+				|| (topleft>c && topright>c && bottomleft<c && bottomright<c))
+			{
+				// points are divided down some line in the middle
+				double left = calcPoint(bottomleft, topleft, c, vPixSpacing[1]);
+				double right = calcPoint(bottomright, topright, c, vPixSpacing[1]);
+					
+				pDC->MoveTo(ToDC(CVectorD<2>(vOrigin[0] + vPixSpacing[0] * (REAL) nX, 
+					vOrigin[1] + vPixSpacing[1] * (REAL) nY + left), mBasis));
+				pDC->LineTo(ToDC(CVectorD<2>(vOrigin[0] + vPixSpacing[0] * (REAL) (nX+1), 
+					vOrigin[1] + vPixSpacing[1] * (REAL) nY + right), mBasis));
+			} 
+
+			//case 1.2 
+			else if((topleft<c && topright>c && bottomleft<c && bottomright>c)
+				|| (topleft>c && topright<c && bottomleft>c && bottomright<c))
+			{
+				// points are divided down some line in the middle
+				double bottom = calcPoint(bottomleft, bottomright, c, vPixSpacing[0]);    
+				double top = calcPoint(topleft, topright, c, vPixSpacing[0]);
+				
+				pDC->MoveTo(ToDC(CVectorD<2>(vOrigin[0] + vPixSpacing[0] * (REAL) nX + bottom, 
+					vOrigin[1] + vPixSpacing[1] * (REAL) nY), mBasis));
+				pDC->LineTo(ToDC(CVectorD<2>(vOrigin[0] + vPixSpacing[0] * (REAL) nX + top, 
+					vOrigin[1] + vPixSpacing[1] * (REAL) (nY + 1)), mBasis));
+			} 
+			
+			//case 2              
+			else if((topleft<c && topright<c && bottomleft<c && bottomright<c)
+				|| (topleft>c && topright>c && bottomleft>c && bottomright>c)) 
+			{
+				// all points below or above the line, so dont do anything
+			}  
+			//case 3.1              
+			else if((topleft>c && topright>c && bottomleft>c && bottomright<c)
+				||(topleft<c && topright<c && bottomleft<c && bottomright>c))
+			{
+				// three points above or below the line and the other the opposite
+				double right = calcPoint(bottomright, topright, c, vPixSpacing[1]);
+				double bottom = calcPoint(bottomleft, bottomright, c, vPixSpacing[0]);
+				
+				pDC->MoveTo(ToDC(CVectorD<2>(vOrigin[0] + vPixSpacing[0] * (REAL) nX + bottom, 
+					vOrigin[1] + vPixSpacing[1] * (REAL) nY), mBasis));
+				pDC->LineTo(ToDC(CVectorD<2>(vOrigin[0] + vPixSpacing[0] * (REAL) (nX+1), 
+					vOrigin[1] + vPixSpacing[1] * (REAL) nY + right), mBasis));
+
+				// linevector.add(new Point3f((float)(i+bottom),(float)j,(float)(k)));
+				// linevector.add(new Point3f((float)(i+vPixSpacing[0]),(float)(j+right), (float)(k)));
+			}  
+			//case 3.2              
+			else if((topleft>c && topright>c && bottomleft<c && bottomright>c)
+				||(topleft<c && topright<c && bottomleft>c && bottomright<c))
+			{
+				// three points above or below the line and the other the opposite
+				double left = calcPoint(bottomleft, topleft, c, vPixSpacing[1]);
+				double bottom = calcPoint(bottomleft, bottomright, c, vPixSpacing[0]);
+				
+				pDC->MoveTo(ToDC(CVectorD<2>(vOrigin[0] + vPixSpacing[0] * (REAL) nX + bottom, 
+					vOrigin[1] + vPixSpacing[1] * (REAL) nY), mBasis));
+				pDC->LineTo(ToDC(CVectorD<2>(vOrigin[0] + vPixSpacing[0] * (REAL) nX, 
+					vOrigin[1] + vPixSpacing[1] * (REAL) nY + left), mBasis));
+
+				// linevector.add(new Point3f((float)(i+bottom),(float)j,(float)(k)));
+				// linevector.add(new Point3f((float)(i),(float)(j+left), (float)(k)));
+			}  
+			//case 3.3             
+			else if((topleft<c && topright>c && bottomleft>c && bottomright>c)
+				||(topleft>c && topright<c && bottomleft<c && bottomright<c))
+			{
+				// three points above or below the line and the other the opposite
+				double left = calcPoint(bottomleft, topleft, c, vPixSpacing[1]);
+				double top = calcPoint(topleft, topright, c, vPixSpacing[0]);
+				
+				pDC->MoveTo(ToDC(CVectorD<2>(vOrigin[0] + vPixSpacing[0] * (REAL) nX, 
+					vOrigin[1] + vPixSpacing[1] * (REAL) nY + left), mBasis));
+				pDC->LineTo(ToDC(CVectorD<2>(vOrigin[0] + vPixSpacing[0] * (REAL) nX + top, 
+					vOrigin[1] + vPixSpacing[1] * (REAL) (nY+1)), mBasis));
+
+				// linevector.add(new Point3f((float)(i),(float)(j+left),(float)(k)));
+				// linevector.add(new Point3f((float)(i+top),(float)(j+vPixSpacing[1]), (float)(k)));
+			}  
+			//case 3.4              
+			else if((topleft>c && topright<c && bottomleft>c && bottomright>c)
+				||(topleft<c && topright>c && bottomleft<c && bottomright<c))
+			{
+				// three points above or below the line and the other the opposite
+				double top = calcPoint(topleft, topright, c, vPixSpacing[0]);
+				double right = calcPoint(bottomright, topright, c, vPixSpacing[1]);
+				
+				pDC->MoveTo(ToDC(CVectorD<2>(vOrigin[0] + vPixSpacing[0] * (REAL) nX + top, 
+					vOrigin[1] + vPixSpacing[1] * (REAL) (nY+1) ), mBasis));
+				pDC->LineTo(ToDC(CVectorD<2>(vOrigin[0] + vPixSpacing[0] * (REAL) (nX+1), 
+					vOrigin[1] + vPixSpacing[1] * (REAL) nY + right), mBasis));
+
+				// linevector.add(new Point3f((float)(i+top),(float)(j+vPixSpacing[1]),(float)(k)));
+				// linevector.add(new Point3f((float)(i+vPixSpacing[0]),(float)(j+right), (float)(k)));
+			}  
+			
+			//case 4.1              
+			else if(topleft>c && topright<c && bottomleft<c && bottomright>c)
+			{
+				//Ambigous case where the corners are diametrically opposed
+				//Test in the middle and decide which way the lines go..
+				double middle = 0.0; // SFS.val(new Vector3d((double)(i+(vPixSpacing[0]/2)), (double)(j+(stepj/2)), 0.0));
+				
+				double top = calcPoint(topleft, topright, c, vPixSpacing[0]);
+				double bottom = calcPoint(bottomleft, bottomright, c, vPixSpacing[0]);
+				double left = calcPoint(bottomleft, topleft, c, vPixSpacing[1]);
+				double right = calcPoint(bottomright, topright, c, vPixSpacing[1]);
+				
+				if(middle < c)
+				{
+					pDC->MoveTo(ToDC(CVectorD<2>(vOrigin[0] + vPixSpacing[0] * (REAL) nX, 
+						vOrigin[1] + vPixSpacing[1] * (REAL) nY + left), mBasis));
+					pDC->LineTo(ToDC(CVectorD<2>(vOrigin[0] + vPixSpacing[0] * (REAL) nX + top, 
+						vOrigin[1] + vPixSpacing[1] * (REAL) (nY+1)), mBasis));
+
+					// linevector.add(new Point3f((float)(i),(float)(j+left),(float)(k)));
+					// linevector.add(new Point3f((float)(i+top),(float)(j+stepj), (float)(k)));
+					
+					pDC->MoveTo(ToDC(CVectorD<2>(vOrigin[0] + vPixSpacing[0] * (REAL) nX + bottom, 
+						vOrigin[1] + vPixSpacing[1] * (REAL) nY), mBasis));
+					pDC->LineTo(ToDC(CVectorD<2>(vOrigin[0] + vPixSpacing[0] * (REAL) (nX+1), 
+						vOrigin[1] + vPixSpacing[1] * (REAL) nY + right), mBasis));
+
+					// linevector.add(new Point3f((float)(i+bottom),(float)(j),(float)(k)));
+					// linevector.add(new Point3f((float)(i+vPixSpacing[0]),(float)(j+right), (float)(k)));  
+				}
+				else if(middle > c)
+				{
+					pDC->MoveTo(ToDC(CVectorD<2>(vOrigin[0] + vPixSpacing[0] * (REAL) nX, 
+						vOrigin[1] + vPixSpacing[1] * (REAL) nY + left), mBasis));
+					pDC->LineTo(ToDC(CVectorD<2>(vOrigin[0] + vPixSpacing[0] * (REAL) nX + bottom, 
+						vOrigin[1] + vPixSpacing[1] * (REAL) nY), mBasis));
+
+					// linevector.add(new Point3f((float)(i),(float)(j+left),(float)(k)));
+					// linevector.add(new Point3f((float)(i+bottom),(float)(j),(float)(k)));
+					
+					pDC->MoveTo(ToDC(CVectorD<2>(vOrigin[0] + vPixSpacing[0] * (REAL) nX + top, 
+						vOrigin[1] + vPixSpacing[1] * (REAL) (nY+1)), mBasis));
+					pDC->LineTo(ToDC(CVectorD<2>(vOrigin[0] + vPixSpacing[0] * (REAL) (nX+1), 
+						vOrigin[1] + vPixSpacing[1] * (REAL) nY + right), mBasis));
+
+					// linevector.add(new Point3f((float)(i+top),(float)(j+stepj), (float)(k)));
+					// linevector.add(new Point3f((float)(i+vPixSpacing[0]),(float)(j+right), (float)(k)));
+				}
+				
+			}
+			//case 4.2
+			else if(topleft<c && topright>c && bottomleft>c && bottomright<c)
+			{
+				//Ambigous case where the corners are diametrically opposed
+				//Test in the middle and decide which way the lines go..
+				double middle = 0.0; // SFS.val(new Vector3d((double)(i+(vPixSpacing[0]/2)), (double)(j+(stepj/2)), 0.0));
+				double top = calcPoint(topleft, topright, c, vPixSpacing[0]);
+				double bottom = calcPoint(bottomleft, bottomright, c, vPixSpacing[0]);
+				double left = calcPoint(bottomleft, topleft, c, vPixSpacing[1]);
+				double right = calcPoint(bottomright, topright, c, vPixSpacing[1]);
+				
+				if(middle<c)
+				{
+					pDC->MoveTo(ToDC(CVectorD<2>(vOrigin[0] + vPixSpacing[0] * (REAL) nX, 
+						vOrigin[1] + vPixSpacing[1] * (REAL) nY + left), mBasis));
+					pDC->LineTo(ToDC(CVectorD<2>(vOrigin[0] + vPixSpacing[0] * (REAL) nX + bottom, 
+						vOrigin[1] + vPixSpacing[1] * (REAL) nY), mBasis));
+
+					// linevector.add(new Point3f((float)(i),(float)(j+left),(float)(k)));
+					// linevector.add(new Point3f((float)(i+bottom),(float)(j), (float)(k)));
+					
+					pDC->MoveTo(ToDC(CVectorD<2>(vOrigin[0] + vPixSpacing[0] * (REAL) nX + top, 
+						vOrigin[1] + vPixSpacing[1] * (REAL) (nY+1)), mBasis));
+					pDC->LineTo(ToDC(CVectorD<2>(vOrigin[0] + vPixSpacing[0] * (REAL) (nX+1), 
+						vOrigin[1] + vPixSpacing[1] * (REAL) nY + right), mBasis));
+
+					// linevector.add(new Point3f((float)(i+top),(float)(j+stepj),(float)(k)));
+					// linevector.add(new Point3f((float)(i+vPixSpacing[0]),(float)(j+right), (float)(k)));   
+				}
+				else if(middle>c)
+				{
+				/*	linevector.add(new Point3f((float)(i),(float)(j+left),(float)(k)));
+					linevector.add(new Point3f((float)(i+top),(float)(j+stepj),(float)(k)));
+					
+					linevector.add(new Point3f((float)(i+bottom),(float)(j), (float)(k)));
+					linevector.add(new Point3f((float)(i+vPixSpacing[0]),(float)(j+right), (float)(k)));   */
+				} 
+				
+			}
+			
+			// nX++; 
+    }//close i-loop
+	
+	// i = imin;
+	// nY++;
+	
+ }//close j-loop
+ 
+ // i = imin;
+ // j = jmin;
+ // myzcounter++;
+ 
+} // close k-loop
+
+
---
+++ Brimstone/PlanarView.h
@@ -0,0 +1,83 @@
+#if !defined(AFX_PLANARVIEW_H__AB0EB4F4_0BF4_4135_935C_FBE17772A6A8__INCLUDED_)
+#define AFX_PLANARVIEW_H__AB0EB4F4_0BF4_4135_935C_FBE17772A6A8__INCLUDED_
+
+#include <Dib.h>
+#include <Volumep.h>
+#include <Structure.h>
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+// PlanarView.h : header file
+//
+
+/////////////////////////////////////////////////////////////////////////////
+// CPlanarView window
+
+class CPlanarView : public CWnd
+{
+// Construction
+public:
+	CPlanarView();
+
+// Attributes
+public:
+	void SetVolume(CVolume<REAL> *pVolume, int nVolumeAt = 0);
+	void SetBasis(const CMatrixD<4>& mBasis);
+	void SetWindowLevel(REAL win, REAL cen, int nVolumeAt = 0);
+	void SetLUT(CArray<COLORREF, COLORREF>& arrLUT, int nVolumeAt = 0);
+
+	void AddStructure(CStructure *pStruct);
+
+// Operations
+public:
+
+// Overrides
+	// ClassWizard generated virtual function overrides
+	//{{AFX_VIRTUAL(CPlanarView)
+	//}}AFX_VIRTUAL
+
+// Implementation
+public:
+	CSeries * m_pSeries;
+	void DrawContours(CDC *pDC);
+	void DrawImages(CDC *pDC);
+	void DrawIsocurves(CVolume<REAL> *pVolume, REAL c, CDC *pDC);
+	REAL m_alpha;
+	void SetAlpha(REAL alpha);
+	CVolume<REAL> * m_pVolume[2];
+	CVolume<REAL> m_volumeResamp[2];
+	REAL m_window[2];
+	REAL m_level[2];
+
+	REAL m_windowStart;
+	REAL m_levelStart;
+	CPoint m_ptWinLevStart;
+	BOOL m_bWindowLeveling;
+
+	CArray<COLORREF, COLORREF> m_arrLUT[2];
+
+	CArray<COLORREF, COLORREF> m_arrPixels;
+	CDib m_dib;
+
+	CTypedPtrArray<CPtrArray, CStructure *> m_arrStructures;
+
+	virtual ~CPlanarView();
+
+	// Generated message map functions
+protected:
+	//{{AFX_MSG(CPlanarView)
+	afx_msg void OnPaint();
+	afx_msg void OnMButtonDown(UINT nFlags, CPoint point);
+	afx_msg void OnMButtonUp(UINT nFlags, CPoint point);
+	afx_msg void OnMouseMove(UINT nFlags, CPoint point);
+	//}}AFX_MSG
+	DECLARE_MESSAGE_MAP()
+};
+
+/////////////////////////////////////////////////////////////////////////////
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_PLANARVIEW_H__AB0EB4F4_0BF4_4135_935C_FBE17772A6A8__INCLUDED_)
