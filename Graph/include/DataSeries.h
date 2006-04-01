3c0213193d803805ae64caf1d332affca53f990e
HP_Administrator <>
2006-04-01 18:41:02 +0000

    added attributes

--- Graph/include/DataSeries.h
+++ Graph/include/DataSeries.h
@@ -2,7 +2,9 @@
 
 #include <VectorD.h>
 #include <MatrixNxM.h>
+
 #include <ModelObject.h>
+#include <Attributes.h>
 
 class CGraph;
 
@@ -13,53 +15,36 @@ public:
 	CDataSeries(void);
 	virtual ~CDataSeries(void);
 
-	COLORREF GetColor() const;
-	void SetColor(COLORREF color);
+	// the object of the data series
+	DECLARE_ATTRIBUTE_PTR(Object, CObject);
 
-	void SetObject(CObject *pObject);
-	CObject * GetObject();
+	// curve attributes
+	DECLARE_ATTRIBUTE_GI(Color, COLORREF);
+	DECLARE_ATTRIBUTE(PenStyle, int);
 
 	// accessors for the data series data
-	const CMatrixNxM<>& GetDataMatrix() const;
+	virtual const CMatrixNxM<>& GetDataMatrix() const;
 	virtual void SetDataMatrix(const CMatrixNxM<>& mData);
 	void AddDataPoint(const CVectorD<2>& vDataPt);
 
 	// flag to indicate whether the data series should have handles
 	//		for interaction
-	BOOL HasHandles() const;
-	void SetHasHandles(BOOL bHandles = TRUE);
+	DECLARE_ATTRIBUTE(HasHandles, bool);
 
 	// accessors to indicate the data series monotonic direction: 
 	//		-1 for decreasing, 
 	//		1 for increasing,
 	//		0 for not monotonic
-	int GetMonotonicDirection() const;
-	void SetMonotonicDirection(int nDirection);
+	DECLARE_ATTRIBUTE(MonotonicDirection, int);
 
 protected:
 	friend CGraph;
 
+public:
 	// my graph (if part of graph
 	CGraph *m_pGraph;
 
-	// the object pointer
-	CObject * m_pObject;
-
 	// use a std vector instead of CArray, because CArray is bad
-	CMatrixNxM<> m_mData;	
+	mutable CMatrixNxM<> m_mData;	
 
-	COLORREF m_color;
-
-	// flag to indicate display of handles for the series
-	BOOL m_bHandles;
-
-	// indicates the data series monotonic direction: 
-	//		-1 for decreasing, 
-	//		1 for increasing,
-	//		0 for not monotonic
-	int m_nMonotonicDirection;
-
-public:
-	// pen style
-	int m_nPenStyle;
 };	// class CDataSeries
